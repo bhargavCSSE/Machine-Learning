2817428212b439beec9599c85bf7136ef7ae51cd
Bhargav <bhargav.csse@gmail.com>
2020-03-21 19:33:59 -0500

    Decision Tree multiclass complete

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import math
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
 class leaf:
     def __init__(self, data):
@@ -42,10 +44,7 @@ def readfile(filename):
             name, value = elem.split(':')
             temp_x[int(name)] = (float(value))
         x = x + [temp_x]
-    x = pd.DataFrame(x).fillna(-1)
-    x = x.assign(label=y)
-    x = x.values.tolist()
-    return x
+    return x, y
 
 def count_classes(rows):
     counts = {}
@@ -88,40 +87,37 @@ def find_best_split(rows):
 
     for col in range(n_features):
         values = set([row[col] for row in rows])
-        for val in values:
-            question = Question(col, val)
+        for elem in values:
+            question = Question(col, elem)
             right, left = split_data(rows, question)
-
-            if len(right) == 0 or len(left) == 0:
-                continue
- 
             gain = infoGain(right, left, current_uncertainty)
             if gain >= best_gain:
                 best_gain, best_question = gain, question
+    
     return best_gain, best_question
 
-def build_tree(rows):
+def grow_tree(rows):
     gain, question = find_best_split(rows)
     if gain == 0:
         return leaf(rows)
 
     true_rows, false_rows = split_data(rows, question)
 
-    true_branch = build_tree(true_rows)
-    false_branch = build_tree(false_rows)
+    true_branch = grow_tree(true_rows)
+    false_branch = grow_tree(false_rows)
 
     return node(question, true_branch, false_branch)
 
-def classify(row, node):
-    if isinstance(node, leaf):
-        return node.y_pred
+def predict(row, tree):
+    if isinstance(tree, leaf):
+        return tree.y_pred
 
-    if node.question.match(row):
-        return classify(row, node.right_node)
+    if tree.question.match(row):
+        return predict(row, tree.right_node)
     else:
-        return classify(row, node.left_node)
+        return predict(row, tree.left_node)
 
-def predict(counts):
+def output_leaf(counts):
     total = sum(counts.values()) * 1.0
     probs = {}
     for label in counts.keys():
@@ -129,13 +125,61 @@ def predict(counts):
     return probs
 
 
+def PerformanceMatrix(y_actual, y_pred):
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for test_instance_result, label in zip(y_pred, y_actual):
+        if ((test_instance_result > 0.5) and (label > 0.5)):
+            tp += 1
+        if ((test_instance_result <= 0.5) and (label <= 0.5)):
+            tn += 1
+        if ((test_instance_result > 0.5) and (label <= 0.5)):
+            fp += 1
+        if ((test_instance_result <= 0.5) and (label > 0.5)):
+            fn += 1
 
-training_data = readfile('Dataset/iris.txt')
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    recall = tp / (tp + fn + 0.00001)
+    precision = tp / (tp + fp + 0.00001)
+    f1 = 2 * (precision * recall) / (precision + recall + 0.00001)
+    print("Accuracy:     ", accuracy)
+    print("Recall:       ", recall)
+    print("Precision:    ", precision)
+    print("F1:           ", f1)
+
+def featureSelect(X, x):
+    mask = list(X.columns.values)
+    x = x.loc[:, mask]
+    return x
+
+# x, y = readfile('Dataset/a4a.txt')
+# x = pd.DataFrame(x).fillna(-1)
+# x = x.assign(label=y)
+# x = x.values.tolist()
+
+# training_data, testing_data, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+train_data, y_train = readfile('Dataset/a4a.txt')
+train_data = pd.DataFrame(train_data).fillna(-1)
+train_data = train_data.assign(label=y_train)
+training_data = train_data.values.tolist()
+
 print("Building the tree")
-my_tree = build_tree(training_data)
+my_tree = grow_tree(training_data)
 print("Done")
-testing_data = readfile('Dataset/iris.txt')
 
+testing_data, y_test = readfile('Dataset/a4a_t.txt')
+testing_data = pd.DataFrame(testing_data).fillna(-1)
+testing_data = featureSelect(train_data, testing_data)
+testing_data = testing_data.assign(label=y_test)
+testing_data = testing_data.values.tolist()
+
+y_pred = []
 for row in testing_data:
-    print("Actual: %s. Predicted: %s" %
-          (row[-1], predict(classify(row, my_tree))))
+    y = (output_leaf(predict(row, my_tree)))
+    for key, value in y.items():
+        y_pred.append(float(key))
+y_pred = np.array(y_pred)
+PerformanceMatrix(y_test, y_pred)
